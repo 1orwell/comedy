c3eea528d9ecbb535edbdb4c6d0b04b8be090076
Phil Ratcliffe <phil@redkestrel.co.uk>
2012-12-23 19:59:11 +0000

    updated path to dg; ignore static files directory

--- comedy/settings.py
+++ comedy/settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', 
-        'NAME': 'comedy.db',             # 
+        'NAME': '/home/elise/comedy/comedy.db',             # 
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
